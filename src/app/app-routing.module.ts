52f8b4555073d1e6f3f9ab31bfc3dc72edcb4794
MasterMind917 <84152987+MasterMind917@users.noreply.github.com>
2022-03-06 22:16:03 +0530

    minor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BillsComponent } from './bills/bills.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { InventoryComponent } from './inventory/inventory.component';
 
@@ -7,6 +8,7 @@ const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'inventory', component: InventoryComponent },
+  { path: 'bills', component: BillsComponent}
 ];
 
 @NgModule({
